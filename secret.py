4673ff9672114e803ddeaa3e7b3f46191041abf8
Paul Boot <paulboot@gmail.com>
2017-12-01 21:30:37 +0100

    Added config file with secrets

---
+++ secret.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#VARIABLES THAT NEED CHANGED
+readonlyusername = 'guest'
+readonlypassword = 'guest'
+
+#Read arp tables from these routers
+routers = ['10.255.255.254', '192.168.254.254']
